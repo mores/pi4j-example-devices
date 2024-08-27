9fca2edde34443301cde0f500bc30357a591f3ff
Stephen More <stephen.more@gmail.com>
2024-08-27 07:48:55 -0400

    Adds in the Spi.

--- src/test/java/com/pi4j/devices/st7789/ST7789Test.java
+++ src/test/java/com/pi4j/devices/st7789/ST7789Test.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
+import com.pi4j.io.spi.Spi;
+import com.pi4j.io.spi.SpiBus;
+import com.pi4j.io.spi.SpiConfig;
 
 public class ST7789Test {
 
@@ -26,5 +29,14 @@ public class ST7789Test {
 
 	public void testOne() {
 		log.info( "testOne" );
+
+		SpiConfig spiConfig = Spi.newConfigBuilder(pi4j)
+			.id("SPI" + 1)
+			.name("ST7789")
+			.baud(24000000)
+			.build();
+
+		Spi spi = pi4j.create(spiConfig);
+
 	}
 }
